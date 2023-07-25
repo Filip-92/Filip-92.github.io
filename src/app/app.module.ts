e20e43b6d25c6bec421e5340ca4e4c5a2f9c4fdb
Filip <filip.peszke.programista@gmail.com>
2023-07-25 10:03:49 +0200

    Publishing the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { ApiService } from './_services/api.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { HourlyWeatherComponent } from './hourly-weather/hourly-weather.component';
+import { WeeklyForecastComponent } from './weekly-forecast/weekly-forecast.component';
+import { ScrollableDirective } from './scrollable.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    CurrentWeatherComponent,
+    HourlyWeatherComponent,
+    WeeklyForecastComponent,
+    ScrollableDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [ ApiService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  secretKey:  'd9726bae9e95fd1aeef20591d8d3c3f3',
+  geoSecretKey: '0aa2eb4af8ce4d4b88e8c8cee2309578'
 };
