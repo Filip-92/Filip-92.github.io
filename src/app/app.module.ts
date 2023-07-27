87393669a9e5b463f1f67e278dd6b940edb47b16
Filip <filip.peszke.programista@gmail.com>
2023-07-27 13:20:28 +0200

    Changing to PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,6 +17,7 @@ import { WeeklyForecastComponent } from './weekly-forecast/weekly-forecast.compo
 import { ScrollableDirective } from './scrollable.directive';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,13 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     ReactiveFormsModule,
     HttpClientModule,
     NgbModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     ApiService,
