cb0fb2586c56b5d1ad98230530dd3935e633a5b9
Filip <filip.peszke.programista@gmail.com>
2023-12-03 19:22:52 +0100

    Some fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  secretKey:  '708fb0f69d76018e2076f62b7e281c0d',
-  geoSecretKey: '0aa2eb4af8ce4d4b88e8c8cee2309578'
+  secretKey:  '${{secrets.OPEN_WEATHER_KEY}}',
+  geoSecretKey: '${{secrets.GEO_COORDS_KEY}}'
 };
